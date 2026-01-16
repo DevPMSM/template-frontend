309a768988c89d9ecab80dd33b0b2756b52ffc65
LucasELopes <lucaslopeslll412@gmail.com>
2026-01-16 09:43:27 -0300

    feat: add prettier

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/storage/image/**",
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/image/**',
       },
       {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,12 +1,12 @@
 export type UserType = {
-  id: string,
-  name: string,
-  email: string,
-  role: string,
-  email_verified_at?: Date,
-  image?: string,
-  last_updated_by: string,
-  created_at: Date,
-  updated_at: Date,
-  deleted_at: Date
-}
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  email_verified_at?: Date;
+  image?: string;
+  last_updated_by: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+};
