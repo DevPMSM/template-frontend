f1f6ed1a12dd38b43f18e4ee0c5664bc7bb046f0
LucasELopes <lucaslopeslll412@gmail.com>
2026-01-20 12:14:41 -0300

    feat: add validator e formatter

---
+++ src/utils/validator.ts
@@ -0,0 +1,40 @@
+// Formata o número de telefone no padrão 999.999.999-99
+export const validateCPF = (cpf: string): boolean => {
+  cpf = cpf.replace(/[^\d]+/g, '');
+
+  // Verifica se tem 11 dígitos e não é uma sequência repetida
+  if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+    return false;
+  }
+
+  // Validação do primeiro dígito verificador
+  let sum = 0;
+
+  for (let i = 0; i < 9; i++) {
+    sum += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+
+  let remainder = 11 - (sum % 11);
+
+  let digit = remainder >= 10 ? 0 : remainder;
+
+  if (digit !== parseInt(cpf.charAt(9))) {
+    return false;
+  }
+
+  // Validação do segundo dígito verificador
+  sum = 0;
+
+  for (let i = 0; i < 10; i++) {
+    sum += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+
+  remainder = 11 - (sum % 11);
+  digit = remainder >= 10 ? 0 : remainder;
+
+  if (digit !== parseInt(cpf.charAt(10))) {
+    return false;
+  }
+
+  return true;
+};
