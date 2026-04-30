fcc32c3f5df8525c4a20f1e4d3f7267b02b23d78
LucasELopes <lucaslopeslll412@gmail.com>
2026-04-30 08:32:20 -0300

    fix: loading animation

--- src/app/(site)/auth/sign-in/page.tsx
+++ src/app/(site)/auth/sign-in/page.tsx
@@ -1,27 +1,22 @@
 "use client";
 
 import logoPref from "@public/logo-pref-notext.png";
-import {
-  FormEvent,
-  useEffect,
-  useState,
-  ChangeEvent,
-} from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
+import { IoChevronBackOutline } from "react-icons/io5";
 import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { Label } from "@/components/label";
-import { Button } from "@/components/button";
-import { Input } from "@/components/input";
-import { MdOutlineEmail } from "react-icons/md";
-import { HiOutlineKey } from "react-icons/hi";
+import { TfiEmail } from "react-icons/tfi";
+import { PiKey } from "react-icons/pi";
 import Loading from "@/components/loading";
+import { Label } from "@/components/label";
+import { Input } from "@/components/input";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { login, error, loading } = useAuth();
+  const { login, error } = useAuth();
   const { isAuthenticated } = useAuthStore();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -38,20 +33,28 @@ export default function SignInPage() {
 
   useEffect(() => {
     if (isAuthenticated()) {
-      router.push("/");
+      router.push("/dashboard");
     }
   }, [isAuthenticated, router]);
 
   return (
-    <div className="font-nunito flex min-h-screen w-full flex-col md:flex-row">
-      <div className="flex items-center justify-center bg-linear-to-t from-[#084B26CC] to-[#22325EB2] px-10 py-16 shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)] max-sm:rounded-b-4xl md:w-7/12 md:rounded-r-4xl">
+    <div className="font-nunito relative flex min-h-screen w-full flex-col md:flex-row">
+      <div className="relative flex items-center justify-center bg-linear-to-t from-[#084B26CC] to-[#22325EB2] px-10 py-16 shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)] max-sm:rounded-b-4xl md:w-7/12 md:rounded-r-4xl">
+        <IoChevronBackOutline
+          className="absolute top-o left-0 transition-all duration-200 hover:scale-110 hover:cursor-pointer md:left-8"
+          size={30}
+          color="#3B5394"
+          onClick={() => {
+            router.back();
+          }}
+        />
         <div className="flex text-white">
           <div className="mr-3 min-w-1 flex-1 rounded-4xl bg-[#B2C5FB] md:w-2"></div>
           <div>
-            <h2 className="mb-2 text-3xl font-black md:text-6xl">
+            <h2 className="mb-2 w-full text-3xl font-black md:text-5xl md:whitespace-nowrap">
               Bem-vindo(a)!
             </h2>
-            <p className="text-nomrmal max-w-lg leading-5 font-semibold md:text-3xl md:leading-8">
+            <p className="max-w-lg text-sm leading-5 font-semibold md:text-3xl md:leading-8">
               Só é possível acessa-lo se já estiver
               cadastrado no sistema.
             </p>
@@ -59,7 +62,7 @@ export default function SignInPage() {
         </div>
       </div>
       <div className="flex flex-1 items-center justify-center">
-        <div className="relative flex w-5/6 flex-col items-center justify-center rounded-2xl bg-[#F9F9F9] py-6 shadow-[inset_0px_4px_4px_0px_rgba(0,0,0,0.25),0px_4px_4px_0px_rgba(0,0,0,0.25)] md:h-152 md:max-w-md">
+        <div className="relative flex w-5/6 flex-col items-center justify-center rounded-2xl bg-[#F9F9F9] py-6 shadow-[inset_0px_4px_4px_0px_rgba(0,0,0,0.25),0px_4px_4px_0px_rgba(0,0,0,0.25)] md:h-152 md:max-w-lg">
           <Image
             src={logoPref}
             alt="logo-prefeitura"
@@ -71,15 +74,15 @@ export default function SignInPage() {
             </p>
             <form
               onSubmit={handleSubmit}
-              className="flex w-72 flex-col justify-center gap-3 max-md:mb-6 md:gap-6"
+              className="w-72rem mb-12 flex flex-col justify-center gap-3 md:gap-6"
             >
-              <Label className="mx-auto flex h-8 w-10/12 items-center gap-2 overflow-hidden rounded-lg border-2 border-[#848484] bg-white px-2 md:h-10 md:w-full">
-                <MdOutlineEmail className="text-[16px] md:text-[20px]" />
+              <Label className="mx-auto flex h-8 w-10/12 items-center gap-2 overflow-hidden rounded-lg border-2 border-[#848484] px-1 md:h-10 md:w-full">
+                <TfiEmail className="text-[16px] md:text-[18px]" />
                 <Input
                   type="email"
                   name="email"
-                  placeholder="Email"
-                  className="h-full w-full border-none px-0 md:text-lg"
+                  placeholder="e-mail"
+                  className="h-full w-full border-none outline-none"
                   defaultValue={email}
                   onChange={(
                     e: ChangeEvent<HTMLInputElement>
@@ -88,14 +91,14 @@ export default function SignInPage() {
                   }}
                 />
               </Label>
-              <Label className="mx-auto flex h-8 w-10/12 items-center gap-2 overflow-hidden rounded-lg border-2 border-[#848484] bg-white px-2 md:h-10 md:w-full">
-                <HiOutlineKey className="text-[16px] md:text-[20px]" />
+              <Label className="mx-auto flex h-8 w-10/12 items-center gap-2 overflow-hidden rounded-lg border-2 border-[#848484] px-1 md:h-10 md:w-full">
+                <PiKey className="text-[16px] md:text-[18px]" />
                 <Input
                   type="password"
                   name="password"
                   placeholder="senha"
                   defaultValue={password}
-                  className="h-full w-full border-none px-0 md:text-lg"
+                  className="h-full w-full border-none outline-none"
                   onChange={(
                     e: ChangeEvent<HTMLInputElement>
                   ) => {
@@ -108,15 +111,16 @@ export default function SignInPage() {
                   {error}
                 </p>
               )}
+
               {!isLoading ? (
-                <Button
+                <button
                   type="submit"
-                  className="m-auto my-4 w-fit cursor-pointer rounded-lg border-0 bg-[#3B5394] px-4 py-1 font-black text-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] transition-all duration-150 hover:scale-105 md:h-12 md:w-44"
+                  className="m-auto my-4 w-fit cursor-pointer rounded-lg bg-[#3B5394] px-4 py-1 font-black text-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] transition-all duration-150 hover:scale-105 md:h-12 md:w-44"
                 >
                   ENTRAR
-                </Button>
+                </button>
               ) : (
-                <Loading className="my-4 px-4 py-1" />
+                <Loading className="mx-auto my-4 h-12 w-8 rounded-lg py-1" />
               )}
             </form>
             <div className="absolute right-0 bottom-0 flex h-16 w-full flex-col items-center justify-center gap-1.5">
